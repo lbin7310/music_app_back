c3493f18afb34fb6bf9ccbb4c1799eb309ab7973
ki_blank <lbin7310@gmail.com>
2019-11-03 23:09:28 +0900

    Add config.json

--- sequelize.js
+++ sequelize.js
@@ -2,16 +2,17 @@ import Sequelize from "sequelize";
 import dotenv from "dotenv";
 import AlbumModel from "./models/Album";
 import SongModel from "./models/Song";
+import config from "./config.json";
 
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || "",
-  process.env.DB_USER || "",
-  process.env.DB_PASSWORD || "",
+  config.DB_NAME || "",
+  config.DB_USER || "",
+  config.DB_PASSWORD || "",
   {
-    host: process.env.DB_HOST || "",
-    port: process.env.DB_PORT || "",
+    host: config.DB_HOST || "",
+    port: config.DB_PORT || "",
     dialect: "mysql",
     pool: {
       max: 10,
