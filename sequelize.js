f1b58aa641b7cd8d4f5de6a2b124d375c4d406f4
ki_blank <lbin7310@gmail.com>
2019-11-04 00:20:30 +0900

    Modify

--- sequelize.js
+++ sequelize.js
@@ -5,6 +5,15 @@ import SongModel from "./models/Song";
 
 dotenv.config({ path: "./.env" });
 
+console.log(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  process.env.DB_HOST,
+  process.env.DB_PORT,
+  " <---------"
+);
+
 const sequelize = new Sequelize(
   process.env.DB_NAME || "",
   process.env.DB_USER || "",
