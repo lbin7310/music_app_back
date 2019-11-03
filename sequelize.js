4d8400b59fd08f5ec34d4b9fc2d8b587293c1f21
ki_blank <lbin7310@gmail.com>
2019-11-03 22:06:30 +0900

    test

--- sequelize.js
+++ sequelize.js
@@ -6,19 +6,19 @@ import SongModel from "./models/Song";
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_NAME || "",
+  process.env.DB_USER || "",
+  process.env.DB_PASSWORD || "",
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "mysql",
-    pool: {
-      max: 10,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
+    host: process.env.DB_HOST || "",
+    port: process.env.DB_PORT || "",
+    dialect: "mysql"
+    // pool: {
+    //   max: 10,
+    //   min: 0,
+    //   acquire: 30000,
+    //   idle: 10000
+    // }
   }
 );
 
