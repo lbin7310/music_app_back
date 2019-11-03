7f40a014e5e5f6394b31cd890e35f9d9d1b8e976
ki_blank <lbin7310@gmail.com>
2019-11-03 22:52:27 +0900

    Modify

--- sequelize.js
+++ sequelize.js
@@ -13,9 +13,6 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST || "",
     port: process.env.DB_PORT || "",
     dialect: "mysql",
-    dialectOptions: {
-      ssh: "Amazon RDS"
-    },
     pool: {
       max: 10,
       min: 0,
