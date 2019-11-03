61a673cd6fbee3fa55257cd89ecaa4bd1ce104be
ki_blank <lbin7310@gmail.com>
2019-11-03 22:17:50 +0900

    Add mysql

--- sequelize.js
+++ sequelize.js
@@ -12,13 +12,13 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST || "",
     port: process.env.DB_PORT || "",
-    dialect: "mysql"
-    // pool: {
-    //   max: 10,
-    //   min: 0,
-    //   acquire: 30000,
-    //   idle: 10000
-    // }
+    dialect: "mysql",
+    pool: {
+      max: 10,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
   }
 );
 
