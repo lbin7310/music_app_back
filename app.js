b41f3e40ad7d9b127870fdf61203a7edaa162c15
ki_blank <lbin7310@gmail.com>
2019-11-04 10:27:52 +0900

    Add package-lock.json

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ import albumRouter from "./routers/albumRouter";
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:3000"],
+  origin: "http://localhost:3000",
   optionsSuccessStatus: 200
 };
 
